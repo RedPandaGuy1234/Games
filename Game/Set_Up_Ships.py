b4a62f03d91df770e7383fb46c9e2a32c8fea019
RedPandaGuy1234 <redpandafluff3@gmail.com>
2026-08-05 19:13:24 -0700

    Update ship setup to include coordinate validation
    Refactor ship variables to use lists for coordinates and add coordinate validation.

--- Game/Set_Up_Ships.py
+++ Game/Set_Up_Ships.py
@@ -1,8 +1,27 @@
-import Locations.py
-Patrol_Boat = 0
-Battleship = 0
-Destroyer = 0
-Sumbarine = 0
-Carrier = 0
+import Locations
+Current_Coordinate = 0
+Patrol_Boat = [0, 0]
+Battleship = [0, 0, 0, 0]
+Destroyer = [0, 0, 0]
+Sumbarine = [0, 0, 0]
+Carrier = [0, 0, 0, 0, 0]
 
-Patrol_Boat = input(
+col_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+def check_coordinates_answer(Coordinate):
+    if Coordinate[0] in col_letters and Coordinate[1:].isdigit():
+        return True
+    else:
+        return False
+
+
+Current_Coordinate = input(
+    "Let's get started! What is the first coordinate you want your patrol boat (2 squares)? "
+    "Each coordinate starts with A to J (please use uppercase), then 1 to 10 with no spaces: "
+)
+
+if check_coordinates_answer(Current_Coordinate):
+    print("Valid coordinate!")
+    Patrol_Boat[0] = Current_Coordinate
+else:
+    print("Invalid coordinate!")
