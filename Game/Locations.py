cbd4925403c97c71dc4e5cfacd3c21e21e35cf50
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-08-06 20:28:43 -0700

    Rename function from change_locations_for_ships to import_ships

--- Game/Locations.py
+++ Game/Locations.py
@@ -1,4 +1,4 @@
-def change_locations_for_ships():
+def import_ships():
   import Set_Up_Ships
 
 A_file = ["Nothing", "Nothing", "Nothing", "Nothing," "Nothing", "Nothing", "Nothing", "Nothing", "Nothing", "Nothing"]
