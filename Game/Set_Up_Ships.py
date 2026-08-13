7df084369391005b4ddabe445ade5a834846721b
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-08-12 20:41:20 -0700

    Change is_coordinate_correct to local variable
    Refactor is_coordinate_correct variable to local scope.

--- Game/Set_Up_Ships.py
+++ Game/Set_Up_Ships.py
@@ -8,12 +8,11 @@ Destroyer = [0, 0, 0]
 Submarine = [0, 0, 0]
 Carrier = [0, 0, 0, 0, 0]
 Used_Coordinates = [0] * 17
-is_coordinate_correct = False
 
 col_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 
 def make_sure_coordinate_is_correct(ship, num, coordinate_num):
-    global is_coordinate_correct
+    is_coordinate_correct = False
     while not is_coordinate_correct:
         Current_Coordinate = input("Invalid coordinate! Remember; the coordinate must have the first part being a letter from A to J, and the second part being 1 to 10. Please re-enter the coordinate.")
         converted_coordinate = check_coordinates_answer(Current_Coordinate)
