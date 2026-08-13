a2fd88ca221b2a9c27a934ac6de7da66236e6786
RedPandaGuy1234 <redpandafluff3@gmail.com>
2026-08-12 21:09:40 -0700

    Fix typo in print statement for coordinate confirmation

--- Game/Set_Up_Ships.py
+++ Game/Set_Up_Ships.py
@@ -61,4 +61,4 @@ Current_Coordinate = input("Okay, you now know how to do this! Now, let's do thi
 converted_coordinate = check_coordinates_answer(Current_Coordinate)
 
 if converted_coordinate:
-        print("This is another correct coordinate
+        print("This is another correct coordinate! Let's keep going through the coordinates.")
