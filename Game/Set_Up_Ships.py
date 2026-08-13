fa28bac66490f19de6e897a3f7a1035d8b2fc1ac
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-08-12 21:05:48 -0700

    Enhance coordinate input handling for Patrol Boat
    Added user prompt for second coordinate input and validation.

--- Game/Set_Up_Ships.py
+++ Game/Set_Up_Ships.py
@@ -27,7 +27,7 @@ def make_sure_coordinate_is_correct(ship, num, coordinate_num):
 def check_coordinates_answer(Coordinate):
     if (isinstance(Coordinate, str) and len(Coordinate) >= 2 
             and Coordinate[0] in col_letters and Coordinate[1:].isdigit()
-            and 1 <= int(Coordinate[1:]) <= 10):
+            and 1 <= int(Coordinate[1:]) <= 10)
         column_number = col_letters.index(Coordinate[0])
         row_number = int(Coordinate[1:])
         return (column_number, row_number)
@@ -50,3 +50,9 @@ if converted_coordinate:
 
 else:
     make_sure_coordinate_is_correct(Patrol_Boat, 0, 0)
+
+Current_Coordinate = input("Okay, you now know how to do this! Now, let's do this with the other coordinate in the patrol boat. Please enter it.")
+converted_coordinate = check_coordinates_answer(Current_Coordinate)
+
+if converted_coordinate:
+        print("This is another correct coordinate
