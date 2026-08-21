ff110ee6e30a3c1012c768c8eef5b3e9893b8a74
RedPandaGuy1234 <redpandafluff3@gmail.com>
2026-08-21 07:09:37 -0700

    Refactor file initialization into a single list
    Replaced individual file lists with a single list of files initialized with 'Nothing'.

--- Game/Locations.py
+++ Game/Locations.py
@@ -6,14 +6,15 @@ def import_ships():
 def put_in_ship_data(ship_letter, ship_num, ship_name, num_of_coordinates):
     pass
 
-
-A_file = ["Nothing"] * 10
-B_file = ["Nothing"] * 10
-C_file = ["Nothing"] * 10
-D_file = ["Nothing"] * 10
-E_file = ["Nothing"] * 10
-F_file = ["Nothing"] * 10
-G_file = ["Nothing"] * 10
-H_file = ["Nothing"] * 10
-I_file = ["Nothing"] * 10
-J_file = ["Nothing"] * 10
+Files = [
+    A_file = ["Nothing"] * 10
+    B_file = ["Nothing"] * 10
+    C_file = ["Nothing"] * 10
+    D_file = ["Nothing"] * 10
+    E_file = ["Nothing"] * 10
+    F_file = ["Nothing"] * 10
+    G_file = ["Nothing"] * 10
+    H_file = ["Nothing"] * 10
+    I_file = ["Nothing"] * 10
+    J_file = ["Nothing"] * 10
+]
