f89d5454dc4ee24b65c00bbf877dbb9329c741c8
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-08-26 12:51:34 -0700

    Add input validation for lower bound in Set_Bounds

--- mainfile/NumberGuesser/Creator.py
+++ mainfile/NumberGuesser/Creator.py
@@ -3,4 +3,6 @@ Low_Bound = 0
 High_Bound = 0
 def Set_Bounds(Bound):  
   Bound = input("Hi! Please say the lower part of the range for creating my number!")
-  if 
+  if Bound.isdigit():
+    print("That is correct!")
+  else:
