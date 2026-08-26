06faa9e41b0c102e2f98ed5e58605d05584e4cdc
RedPandaGuy1234 <redpandaguy123@gmail.com>
2026-08-26 16:44:07 -0700

    Update Creator.py

--- mainfile/NumberGuesser/Creator.py
+++ mainfile/NumberGuesser/Creator.py
@@ -1,12 +1,15 @@
 Number = 0
 Low_Bound = 0
 High_Bound = 0
-def Set_Bounds(Bound): 
+def Set_Bounds(Bound, range): 
   is_Correct = False 
   while not is_Correct:
-    Bound = input("Hi! Please say the lower part of the range for creating my number!")
+    Bound = input("Hi! Please say the (range) part of the range for creating my number!")
     if Bound.isdigit():
       print("That is correct!")
       is_Correct = True
     else:
       print("Please try again")
+
+Set_Bounds(Low_Bound, lower)
+
