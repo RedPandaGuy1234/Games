5a68b7d85740684f9629c8d7c9818494b319bdbb
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-08-26 18:00:42 -0700

    Update input prompt to use f-string formatting

--- mainfile/NumberGuesser/Creator.py
+++ mainfile/NumberGuesser/Creator.py
@@ -4,7 +4,7 @@ High_Bound = 0
 def Set_Bounds(Bound, range): 
   is_Correct = False 
   while not is_Correct:
-    Bound = input("Hi! Please say the (range) part of the range for creating my number!")
+    Bound = input(f"Hi! Please say the {range} part of the range for creating my number!")
     if Bound.isdigit():
       print("That is correct!")
       is_Correct = True
