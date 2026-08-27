19db7b8986292c7e439442a1bebbda6c8433975d
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-08-26 18:14:36 -0700

    Improve input prompts and validate bounds
    Refactored input prompts for clarity and added validation for bounds.

--- mainfile/NumberGuesser/Creator.py
+++ mainfile/NumberGuesser/Creator.py
@@ -1,15 +1,23 @@
 Number = 0
 Low_Bound = 0
 High_Bound = 0
+is_low_more = False
 def Set_Bounds(Bound, range): 
   is_Correct = False 
   while not is_Correct:
-    Bound = input(f"Hi! Please say the {range} part of the range for creating my number!")
+    Bound = input(f"Please say the {range} part of the range for creating my number!")
     if Bound.isdigit():
-      print("That is correct!")
+      print("Okay!")
       is_Correct = True
     else:
-      print("Please try again")
+      print("This was not a number. Please try again.")
 
+print("Hi!")
 Set_Bounds(Low_Bound, "lower")
+Set_Bounds(High_Bound, "higher")
+while not is_low_more:
+  if Low_Bound > High_Bound:
+    print("The lower part of the range is greater than the higher one. Please restart.")
+    Set_Bounds(Low_Bound, "lower")
+    Set_Bounds(High_Bound, "higher")
 
