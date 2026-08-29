c1074035e2e11bffecf74a0315a4d9c167533b7c
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-08-28 18:10:48 -0700

    Refactor Set_Bounds and guess_number functions

--- mainfile/NumberGuesser/Creator.py
+++ mainfile/NumberGuesser/Creator.py
@@ -6,49 +6,54 @@ guess_result = 0
 Low_Bound = 0
 High_Bound = 0
 is_low_more = False
-def Set_Bounds(Bound, range): 
-  is_Correct = False 
-  while not is_Correct:
-    Bound = input(f"Please say the {range} part of the range for creating my number!")
-    if Bound.isdigit():
-      print("Okay!")
-      is_Correct = True
-      return int(Bound)
-    else:
-      print("This was not an integer. Please try again.")
+
+
+def Set_Bounds(Bound, range):
+    is_Correct = False
+    while not is_Correct:
+        Bound = input(f"Please say the {range} part of the range for creating my number!")
+        if Bound.isdigit():
+            print("Okay!")
+            is_Correct = True
+            return int(Bound)
+        else:
+            print("This was not an integer. Please try again.")
+
 
 def guess_number(Number, guessamount):
-  correct = False
-  guessamount = guessamount + 1
-  guessresult = "start"
-  Guess = 0
-  while not correct:
-    Guess = input("Please tell me your guess!")
-    if Guess.isdigit():
-      if Guess == Number:
-        guessresult = "Correct"
-        correct = True
-        return guessresult, guessamount
-      elif Guess > Number:
-        guessresult = "High"
-        return guessresult, guessamount
-      elif Guess < Number:
-        guessresult = "low"
-        return guessresult, guessamount
-        
+    correct = False
+    guessamount = guessamount + 1
+    guessresult = "start"
+    Guess = 0
+    while not correct:
+        Guess = input("Please tell me your guess!")
+        if Guess.isdigit():
+            Guess = int(Guess)
+            if Guess == Number:
+                guessresult = "Correct"
+                return guessamount
+            elif Guess > Number:
+                guessresult = "High"
+                print("Too high!")
+            elif Guess < Number:
+                guessresult = "low"
+                print("Too low!")
+        else:
+            print("This was not an integer. Please try again.")
+
 
 print("Hi!")
 Low_Bound = Set_Bounds(Low_Bound, "lower")
 High_Bound = Set_Bounds(High_Bound, "higher")
 is_low_more = False
 while not is_low_more:
-  if Low_Bound >= High_Bound:
-    print("The lower part of the range is greater or equal to the higher one. Please restart.")
-    Low_Bound = Set_Bounds(Low_Bound, "lower")
-    High_Bound = Set_Bounds(High_Bound, "higher")
-  else: 
-    is_low_more = True
-    
+    if Low_Bound >= High_Bound:
+        print("The lower part of the range is greater or equal to the higher one. Please restart.")
+        Low_Bound = Set_Bounds(Low_Bound, "lower")
+        High_Bound = Set_Bounds(High_Bound, "higher")
+    else:
+        is_low_more = True
+
 Number = random.randint(Low_Bound, High_Bound)
 print(f"I picked a number between {Low_Bound} and {High_Bound}.")
-guess_result, guesses = guess_number(Number, guesses)
+guesses = guess_number(Number, guesses)
