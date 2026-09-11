917dd9f37e3048d8850ec0e12b7bc8cfbd16e2f6
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-09-10 21:21:56 -0700

    Feat: Add functionality to load valid words from URL
    Fetch valid words from a remote text file and handle errors.

---
+++ production_stuff/Wordle/Wordle.py
@@ -0,0 +1,10 @@
+import Valid_Words.txt
+
+# Change this to the actual raw file later, once this is finished
+txt_url = "https://raw.githubusercontent.com/RedPandaGuy1234/Games/refs/heads/main/production_stuff/Wordle/Valid_Words.txt"
+response = requests.get(txt_url)
+
+if response.status_code == 200:
+    Valid_Words = response.text.splitlines()
+else:
+    Valid_Words = []
