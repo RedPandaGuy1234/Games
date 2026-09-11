d3521c117ae9735253a1056b3f61c7b01dd6777f
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-09-10 21:23:16 -0700

    chore: Update Valid_Words.txt URL and add test print
    Updated URL for Valid_Words.txt and added test print statement.

--- production_stuff/Wordle/Wordle.py
+++ production_stuff/Wordle/Wordle.py
@@ -1,6 +1,7 @@
 import Valid_Words.txt
 
-# Change this to the actual raw file later, once this is finished
+# Change this to the actual raw file later, once this is finished.
+
 txt_url = "https://raw.githubusercontent.com/RedPandaGuy1234/Games/refs/heads/main/production_stuff/Wordle/Valid_Words.txt"
 response = requests.get(txt_url)
 
@@ -8,3 +9,6 @@ if response.status_code == 200:
     Valid_Words = response.text.splitlines()
 else:
     Valid_Words = []
+
+# This is a test to see if it works, will be deleted later.
+print(Valid_Words)
