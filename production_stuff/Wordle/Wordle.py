45347d99217f1db860af7adda3b9a2ae42b8f01d
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-09-11 17:10:34 -0700

    feat: Implement word bank retrieval from external source
    Added functionality to fetch word bank from a URL and handle errors.

--- production_stuff/Wordle/Wordle.py
+++ production_stuff/Wordle/Wordle.py
@@ -10,3 +10,13 @@ else:
     Valid_Words = []
     print("This code has failed because the list for valid words did not show up. Please alert RedPandaGuy1234 of this. Thank you!")
     sys.exit(1)
+
+txt_url = "https://raw.githubusercontent.com/RedPandaGuy1234/Games/refs/heads/main/production_stuff/Wordle/Word_Bank.txt"
+response = requests.get(txt_url)
+
+if response.status_code == 200:
+    Word_bank = response.text.splitlines()
+else:
+    Word_bank = []
+    print("This code has failed because the list for the word bank did not show up. Please alert RedPandaGuy1234 of this. Thank you!")
+    sys.exit(1)
