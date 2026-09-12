74fa3910297c8330b37259466257b90e9bae7893
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-09-11 17:28:49 -0700

    feat: Implement word guessing and validation function
    Added a function to handle user word guesses and validate them against the word bank.

--- production_stuff/Wordle/Wordle.py
+++ production_stuff/Wordle/Wordle.py
@@ -28,3 +28,8 @@ else:
 
 Word = Word_bank[random.randint(1, 2314)]
 
+def guess_word():
+guessed_word = input("Hello! Please enter your word, and use all lowercase.")
+if guessed_word not in Valid_Words:
+    print("Sorry, but either you used an uppercase character, it is not in the Valid word bank (this is a limited word bank, and does not have every single five letter work in the world), or this is not a word. Please try again.")
+    guess_word()
