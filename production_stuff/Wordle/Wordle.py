3d455b08e6d343da763d44c98fda832f706c331d
RedPandaGuy1234 <254939784+RedPandaGuy1234@users.noreply.github.com>
2026-09-11 17:32:05 -0700

    fix: Enhance input validation error message
    Updated error message to include space input check.

--- production_stuff/Wordle/Wordle.py
+++ production_stuff/Wordle/Wordle.py
@@ -32,7 +32,7 @@ Guess = "This is a filler!"
 def guess_word():
     guessed_word = input("Hello! Please enter your word, and use all lowercase.")
     if guessed_word not in Valid_Words:
-        print("Sorry, but either you used an uppercase character, it is not in the Valid word bank (this is a limited word bank, and does not have every single five letter work in the world), or this is not a word. Please try again.")
+        print("Sorry, but either you used an uppercase character, it is not in the Valid word bank (this is a limited word bank, and does not have every single five letter work in the world), you typed a space anywhere, or this is not a word. Please try again.")
         guess_word()
     else:
         return guessed_word
